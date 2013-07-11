42051209d5b3c9e6e0b7257adf702012ef05e24b
Andre Smith <lordsilent@gmail.com>
2013-07-11 02:40:45 -0500

    Added texts table

--- IRHE/db/schema.rb
+++ IRHE/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710052737) do
+ActiveRecord::Schema.define(version: 20130711072311) do
+
+  create_table "texts", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "language"
+    t.string   "tags",                       array: true
+    t.boolean  "share",      default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
