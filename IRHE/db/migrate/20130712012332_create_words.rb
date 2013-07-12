8ab313389f4149a40748a3955e028c4416391723
Andre Smith <lordsilent@gmail.com>
2013-07-11 20:26:27 -0500

    Added user, text, and word models

---
+++ IRHE/db/migrate/20130712012332_create_words.rb
@@ -0,0 +1,15 @@
+class CreateWords < ActiveRecord::Migration
+  def change
+    create_table :words do |t|
+      t.string :term
+      t.string :reading
+      t.string :definition
+      t.string :sentence
+      t.string :tags, :array => true
+      t.integer :user_id
+      t.integer :text_id
+
+      t.timestamps
+    end
+  end
+end
--- IRHE/db/schema.rb
+++ IRHE/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130711074604) do
+ActiveRecord::Schema.define(version: 20130712012332) do
 
   create_table "texts", force: true do |t|
     t.integer  "user_id"
