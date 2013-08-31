7cb2c90c96d2d0457ff6338442bda1026694d8e1
Andre Smith <lordsilent@gmail.com>
2013-08-31 04:53:31 -0500

    cleanup

--- IRHE/db/migrate/20130728035509_add_shared_to_words.rb
+++
@@ -1,5 +0,0 @@
-class AddSharedToWords < ActiveRecord::Migration
-  def change
-    add_column :words, :shared, :boolean, default: false
-  end
-end
--- IRHE/spec/support/utilities.rb
+++
@@ -1,6 +0,0 @@
-include ApplicationHelper
-
-def sign_in(user)
-  OmniAuth.config.test_mode = true
-  visit signin_path
-end
