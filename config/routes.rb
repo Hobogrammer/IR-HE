e67c338af74179d3eb7ac08231e70896d3131d47
Andre Smith <lordsilent@gmail.com>
2014-03-24 23:01:11 -0700

    Tabbing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 IRHE::Application.routes.draw do
 
-  mount RedactorRails::Engine => '/redactor_rails'
+mount RedactorRails::Engine => '/redactor_rails'
 root to: 'static_pages#home'
 
 resources :users , :texts, :words
 
-namespace :users do 
-  resources :texts, :words
-end
+  namespace :users do
+    resources :texts, :words
+  end
 
-get '/signin' => redirect("/auth/twitter")
-get '/signout', to: "sessions#destroy", via: :delete
-get "auth/twitter/callback", to: "sessions#create"
-get "/panel", to: "users#main", :as => 'panel'
-post "/search", to: "texts#wordsearch"
-post "/dic", to: "texts#lookup"
+  get '/signin' => redirect("/auth/twitter")
+  get '/signout', to: "sessions#destroy", via: :delete
+  get "auth/twitter/callback", to: "sessions#create"
+  get "/panel", to: "users#main", :as => 'panel'
+  post "/search", to: "texts#wordsearch"
+  post "/dic", to: "texts#lookup"
 end
