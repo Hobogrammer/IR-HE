b22a968d8c054a23dec4cf51df68780d603f4a20
Andre Smith <lordsilent@gmail.com>
2014-09-12 00:09:11 -0700

    Start on accepting paragraphs

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,4 +1,13 @@
 class Paragraph < ActiveRecord::Base
   belongs_to :user
   belongs_to :text
+
+  def self.process(text,content)
+    content_array = content.split(/\n/)
+
+    content_aray.each do |contents|
+      text.paragraph.create(content: contents)
+    end
+    true
+  end
 end
