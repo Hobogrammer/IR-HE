fff6b396786e53c594a795a0b1e7a26465bc9ea4
Andre Smith <lordsilent@gmail.com>
2014-12-03 23:47:27 -0800

    Add user_id foreign_key field to texts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202073106) do
+ActiveRecord::Schema.define(version: 20141202073303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141202073106) do
     t.boolean  "shared"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
